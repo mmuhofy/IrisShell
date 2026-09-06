d1a68c18536c7c0db68a428832f21a21f0c0b0fc
Muhofy <muhofy@iris.local>
2026-09-06 20:30:08 +0000

    fix(data): add missing first() import for Flow in SessionManagerAdapter

--- data/src/main/kotlin/com/iris/irisshell/data/session/SessionManagerAdapter.kt
+++ data/src/main/kotlin/com/iris/irisshell/data/session/SessionManagerAdapter.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
