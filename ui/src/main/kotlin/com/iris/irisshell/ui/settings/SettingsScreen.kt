70a69158ef077e17cc21cc1a7050ac9780fdc047
Muhofy <muhofy@iris.local>
2026-09-11 13:36:24 +0000

    fix: fix Settings

--- ui/src/main/kotlin/com/iris/irisshell/ui/settings/SettingsScreen.kt
+++ ui/src/main/kotlin/com/iris/irisshell/ui/settings/SettingsScreen.kt
@@ -15,8 +15,8 @@ import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.icons.Icons
-import androidx.compose.material3.icons.filled.ArrowBack
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
