dd474b861f201db56b99a922f82a7c14328abea7
Muhofy <muhofy@iris.local>
2026-09-12 12:40:13 +0000

    fix(settings): add widthIn import

--- ui/src/main/kotlin/com/iris/irisshell/ui/settings/SettingsComponents.kt
+++ ui/src/main/kotlin/com/iris/irisshell/ui/settings/SettingsComponents.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
